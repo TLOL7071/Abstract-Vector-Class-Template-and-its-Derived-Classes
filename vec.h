8e70af5641acfefd27c9df2b0521246da929158a
7071 <3463166479.qq.com>
2024-10-20 13:22:58 +0000

    基类

---
+++ vec.h
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class basic_vec{
+protected:
+    T *ptr;
+    T *start;
+    T *end;
+    //新加了定位指针
+    int size;
+    int capacity;
+    basic_vec(int cap);//初始化
+
+public:
+    virtual bool push_back(T)=0;
+    virtual bool resize(int)=0;
+    virtual bool pop()=0;
+    virtual T at(int pos)=0;
+};
+template<typename T>
+basic_vec<T>::basic_vec(int cap):size(0),capacity(cap),ptr(new T[capacity]),start(ptr),end(ptr){}
+
+
