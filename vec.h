dd335ac4edb09c88990168a0d4010a6ff44c478c
7071 <3463166479.qq.com>
2024-10-21 17:36:02 +0000

    修改虚函数

--- vec.h
+++ vec.h
@@ -1,6 +1,7 @@
+#ifndef BASIC_VEC
+#define BASIC_VEC
 #include <iostream>
 using namespace std;
-
 template<typename T>
 class basic_vec{
 protected:
@@ -12,13 +13,10 @@ protected:
     int capacity;
     basic_vec(int cap);//初始化
 
-public:
-    virtual bool push_back(T)=0;
-    virtual bool resize(int)=0;
-    virtual bool pop()=0;
-    virtual T at(int pos)=0;
+    virtual input(istream)=0 const;
+    virtual output(ostream)=0 const;
 };
 template<typename T>
 basic_vec<T>::basic_vec(int cap):size(0),capacity(cap),ptr(new T[capacity]),start(ptr),end(ptr){}
 
-
+#endif
